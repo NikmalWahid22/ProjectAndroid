e84119f224550343fb4a55ffce01e4348846f4ae
NikmalWahid22 <muhamadnikmalwahid@gmail.com>
2026-06-27 19:18:54 +0700

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,45 +1,67 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.campeat.app"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.campeat.app"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.activity:activity:1.8.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    implementation("com.google.android.gms:play-services-location:21.3.0")
+    // ROOM DATABASE
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation(libs.firebase.database)
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("com.google.firebase:firebase-auth")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
+
+    // LOCATION
     implementation("com.google.android.gms:play-services-location:21.0.1")
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+
+    // FIREBASE
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-database")
+
+    // lottiflies
+    implementation("com.airbnb.android:lottie:6.6.7")
+
+    // SSO
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
+
+    // AI assistant
+    implementation("com.google.ai.client.generativeai:generativeai:0.9.0")
+
+    implementation("com.google.guava:guava:31.1-android")
 }
